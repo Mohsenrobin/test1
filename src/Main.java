2029a65a8dfb473069498855533ded9b95c8517e
Mohsen <63107432+Mohsenrobin@users.noreply.github.com>
2022-10-10 17:15:52 +0100

    32

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Inside the Main");
+        System.out.println("Indeed");
+
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
